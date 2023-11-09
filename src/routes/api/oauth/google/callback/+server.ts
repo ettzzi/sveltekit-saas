63932944267560efb02f74123c87c29b037986e0
Enrico Mattiazzi <enrico.mattiazzi@gmail.com>
2023-11-09 21:29:16 +0000

    Add redirect

--- src/routes/api/oauth/google/callback/+server.ts
+++ src/routes/api/oauth/google/callback/+server.ts
@@ -1,4 +1,3 @@
-
 import prisma from '$lib/prisma.js';
 import { auth, googleAuth } from '$lib/server/lucia';
 import { OAuthRequestError } from '@lucia-auth/oauth';
@@ -8,8 +7,6 @@ export const GET = async ({ url, cookies, locals }) => {
 	const storedState = cookies.get('google_oauth_state');
 	const state = url.searchParams.get('state');
 	const code = url.searchParams.get('code');
-
-
 	if (!storedState || !state || storedState !== state || !code) {
 		return new Response(null, {
 			status: 400
@@ -27,20 +24,19 @@ export const GET = async ({ url, cookies, locals }) => {
 				return existingUser;
 			}
 
-            if (!googleUser.email_verified) {
-                throw error(400, 'Email not verified')
-            }
+			if (!googleUser.email_verified) {
+				throw error(400, 'Email not verified');
+			}
 
 			if (!googleUser.email) {
 				throw error(400, 'Google email not found');
 			}
 
 			const existingDatabaseUserWithEmail = await prisma.user.findUnique({
-                where: {
-                    email: googleUser.email
-                }
-            });
-
+				where: {
+					email: googleUser.email
+				}
+			});
 
 			if (existingDatabaseUserWithEmail) {
 				const user = auth.transformDatabaseUser(existingDatabaseUserWithEmail);
@@ -50,7 +46,7 @@ export const GET = async ({ url, cookies, locals }) => {
 
 			return await createUser({
 				attributes: {
-                    name: googleUser.name,
+					name: googleUser.name,
 					email: googleUser.email,
 					email_verified: Boolean(googleUser.email_verified) || false
 				}
@@ -58,20 +54,21 @@ export const GET = async ({ url, cookies, locals }) => {
 		};
 
 		const user = await getUser();
-        const session = await auth.createSession({
+		const session = await auth.createSession({
 			userId: user.userId,
 			attributes: {}
 		});
+
 		locals.auth.setSession(session);
+		const redirectTo = cookies.get('redirectTo');
 		return new Response(null, {
 			status: 302,
 			headers: {
-				Location: "/dashboard"
+				Location: redirectTo ?? '/'
 			}
 		});
 	} catch (e) {
 		if (e instanceof OAuthRequestError) {
-
 			return new Response(null, {
 				status: 400
 			});
